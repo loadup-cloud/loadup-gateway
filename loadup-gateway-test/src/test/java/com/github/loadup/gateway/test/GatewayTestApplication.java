49e2aa3d4b484c5ba7564fee4cc4dbd8d6fb0fbd
Laysan <lslvxy@gmail.com>
2025-12-04 17:03:25 +0800

    update

---
+++ loadup-gateway-test/src/test/java/com/github/loadup/gateway/test/GatewayTestApplication.java
@@ -0,0 +1,44 @@
+package com.github.loadup.gateway.test;
+
+/*-
+ * #%L
+ * LoadUp Gateway Test
+ * %%
+ * Copyright (C) 2025 LoadUp Gateway Authors
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Test application entry point for tests
+ */
+@SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.github.loadup.gateway.core",
+        "com.github.loadup.gateway.facade",
+        "com.github.loadup.gateway.plugins",
+        "com.github.loadup.gateway.test"
+})
+public class GatewayTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayTestApplication.class, args);
+    }
+}
